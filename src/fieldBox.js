8db83c99d3ee1a5545e41c48b2dee25aa7687242
Jon Zachary <jontzachary@gmail.com>
2016-12-15 10:51:55 -0500

    add randomized boxes, improve fieldbox class

--- src/fieldBox.js
+++ src/fieldBox.js
@@ -1,6 +1,6 @@
 class FieldBox {
   constructor(){
-    this.length = null;
+    this.sideLength = null;
     this.xCoord = null;
     this.yCoord = null;
     this.sideColored = null;
@@ -19,12 +19,12 @@ class FieldBox {
   setLength() {
     //I know 64-32 = 32 ;) I just like keeping it this way because it
     //shows the range params.
-    this.length = Math.floor(Math.random()*(64-32) + 32);
+    this.sideLength = Math.floor(Math.random()*(64-32) + 32);
   }
 
   setCoords() {
     this.xCoord = Math.floor(Math.random()*(970-30) + 30);
-    this.yCoord = Math.floor(Math.random()*(570-30) + 30);
+    this.yCoord = Math.floor(Math.random()*(400-30) + 30);
   }
 
   pickSide(){
@@ -35,4 +35,48 @@ class FieldBox {
 
   }
 
+  drawBox(){
+    let aBox = $('<div class=fieldBox></div>');
+
+
+    $(aBox).css({'height':this.sideLength});
+    $(aBox).css({'width':this.sideLength});
+
+    $(aBox).css({'top':this.yCoord});
+    $(aBox).css({'left':this.xCoord});
+
+    $(aBox).appendTo('body');
+
+
+  }
+
 }
+
+ let box1 = new FieldBox();
+ let box2 = new FieldBox();
+ let box3 = new FieldBox();
+ let box4 = new FieldBox();
+
+
+ box1.drawBox();
+ box2.drawBox();
+ box3.drawBox();
+ box4.drawBox();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
